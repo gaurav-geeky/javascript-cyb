01fdaa7c84e8e4dc21cc639f5f3c2e5c9e02014d
gaurav-geeky <gauravmpreat@gmail.com>
2025-04-23 21:44:45 +0530

    number method

---
+++ april23/script23.js
@@ -0,0 +1,23 @@
+// Math methods 
+// when 1 to limit then   + 1  other wise from 0  to no add 
+
+var x = Math.floor(Math.random() * 100) + 1; 
+console.log(x);
+
+var y = Math.floor(Math.random() * 50) + 1;  
+console.log(y);
+
+let sum = (x+y); 
+console.log(sum);
+
+let difference = (x-y); 
+console.log(difference); 
+
+
+console.log(out); 
+
+
+
+
+
+
--- my own practice/scriptprac.js
+++ my own practice/scriptprac.js
@@ -41,6 +41,79 @@ console.log(Math.random());
  // value comes always b/w 0 - 1.  mostly  decimal .000
 
 
+//           23  APRIL  2025  
+
+// numbers and maths in javascript  / 
+
+const score = 200 
+console.log(score);
+
+const balance = new Number(100) /////////
+console.log(balance);
+
+console.log(balance.toString());   //100
+console.log(balance.length); // undefined 
+
+console.log(balance.toFixed(2));   // till 2 after decimal      
+// 100.00
+
+const otNum = 23.8955 
+console.log(otNum.toPrecision(3));  
+// 23.9     give precise value with rounds off 
+// should be used carefully 
+
+const hundereds = 1000000 
+console.log(hundereds.toLocaleString()); 
+// 10,00,000    value acc to US stands  hund thousands ...
+ console.log(hundereds.toLocaleString('en-IN'));     
+// en-IN acc to indian standards
+
+//                          maths                   //
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));   // 4   __mostly round used__
+console.log(Math.round(4.6));         // 5 round off
+
+// if we want to choose smaller or bigger value itself then use 
+console.log(Math.ceil(4.2));    // ceil  highest value
+console.log(Math.floor(4.9));   // floor lowest value
+
+console.log(Math.sqrt(9)); 
+
+//  Where we will use these Math methos ???   // here 
+console.log(Math.random()); 
+ // value comes always b/w 0 - 1.  mostly  decimal .000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
